30f0ec43a71018a24ca743e04e9d8241b8dbcb53
SJKim <hedro0458@gmail.com>
2022-03-14 17:23:34 +0900

    refactor 92334

--- code/S-J-Kim/week2/kakao_92334.py
+++ code/S-J-Kim/week2/kakao_92334.py
@@ -1,28 +1,16 @@
 def solution(id_list, report, k):
 
-    report_table = {}
-    reported_count = {reportee: 0 for reportee in id_list}
+    reported_count = {reportee: set([]) for reportee in id_list}
     mail_count = {reporter: 0 for reporter in id_list}
 
     for single_report in report:
         reporter, reportee = single_report.split()
 
-        if reporter in report_table.keys():
-            if reportee not in report_table[reporter]:
-                report_table[reporter].add(reportee)
-                reported_count[reportee] += 1
+        reported_count[reportee].add(reporter)
 
-        else:
-            report_table[reporter] = set([reportee])
-            reported_count[reportee] += 1
-
-    banned = list(
-        map(lambda x: x[0], (filter(lambda x: x[1] >= k, reported_count.items())))
-    )
-
-    for (reporter, reportee) in report_table.items():
-        for reported_user in reportee:
-            if reported_user in banned:
+    for (reportee, reporters) in reported_count.items():
+        if len(reporters) >= k:
+            for reporter in reporters:
                 mail_count[reporter] += 1
 
     return list(mail_count.values())
